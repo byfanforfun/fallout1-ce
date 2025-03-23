8ec91e95eaa93bb95980433b055b9274c58894c0
byfanforfun <byfanforfun@proton.me>
2025-03-23 18:05:08 +0000

    Add custom kiosk.cfg config definition

---
+++ src/game/gkioskconf.h
@@ -0,0 +1,20 @@
+#ifndef FALLOUT_GAME_GKIOSKCONF_H_
+#define FALLOUT_GAME_GKIOSKCONF_H_
+
+#include "game/config.h"
+
+namespace fallout {
+
+#define KIOSK_CONFIG_FILE_NAME "kiosk.cfg"
+
+#define KIOSK_CONFIG_GAME_KEY	"game"
+
+extern Config kiosk_config;
+
+bool gkioskconf_init();
+bool gkioskconf_save();
+bool gkioskconf_exit(bool shouldSave);
+
+} // namespace fallout
+
+#endif /* FALLOUT_GAME_GKIOSKCONF_H_ */
