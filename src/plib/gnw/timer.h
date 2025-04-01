eecfe4957d4ac9cc6786a1733e103c4a37ee1a29
byfanforfun <byfanforfun@proton.me>
2025-04-01 23:27:20 +0000

    Add game timer

---
+++ src/plib/gnw/timer.h
@@ -0,0 +1,28 @@
+#ifndef FALLOUT_PLIB_GNW_TIMER_H_
+#define FALLOUT_PLIB_GNW_TIMER_H_
+
+namespace fallout {
+
+#define TIMERS_MAX 16
+
+typedef int(*timer_call)(int a1);
+
+struct Timer {
+    timer_call func_ptr;
+    int timeout;
+    int is_fire;
+    int a1;
+};
+
+void timer_tick();
+void timer_init();
+void timer_start();
+void timer_stop();
+void timer_reset();
+int timer_create(int timer, timer_call func_ptr, int timeout, int a1);
+int timer_delete(int timer);
+
+
+}
+
+#endif /* FALLOUT_PLIB_GNW_TIMER_H_ */
