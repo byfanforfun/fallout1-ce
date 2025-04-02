dfd1fe18cca1cf26941141b5cc800da6b3b7b0de
byfanforfun <byfanforfun@proton.me>
2025-04-02 21:28:15 +0000

    Impliment inactive death timer

---
+++ src/game/timer_inactive.h
@@ -0,0 +1,15 @@
+#ifndef FALLOUT_GAME_TIMER_INACTIVE_H_
+#define FALLOUT_GAME_TIMER_INACTIVE_H_
+
+namespace fallout {
+
+#define TIMER_INACTIVE_DEATH    0
+#define TIMER_INACTIVE_ATT_1    1
+#define TIMER_INACTIVE_ATT_2    2
+#define TIMER_INACTIVE_ATT_3    3
+
+void timer_inactive_init();
+
+}
+
+#endif /* FALLOUT_GAME_TIMER_INACTIVE_H_ */
