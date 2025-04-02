23b9dda02ae99546fb642b896230233171aab29f
ByFanForFun <byfanforfun@proton.me>
2025-04-02 21:24:45 +0000

    Add kiosk conf inactive timer related keys

--- src/game/gkioskconf.h
+++ src/game/gkioskconf.h
@@ -11,6 +11,10 @@ namespace fallout {
 #define KIOSK_CONFIG_EXP_START_KEY "exp_start"
 #define KIOSK_CONFIG_ENABLE_SAVELOAD   "saveload"
 #define KIOSK_CONFIG_ENABLE_OPTIONS   "options"
+#define KIOSK_CONFIG_TIMER_INACTIVE_1   "inact1"
+#define KIOSK_CONFIG_TIMER_INACTIVE_2   "inact2"
+#define KIOSK_CONFIG_TIMER_INACTIVE_3   "inact3"
+#define KIOSK_CONFIG_TIMER_INACTIVE_F   "inact"
 
 extern Config kiosk_config;
 extern int gconfig_saveload_allowed;
