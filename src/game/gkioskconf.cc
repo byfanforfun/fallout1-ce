be830fec3f48fd759ddda2e30a566ac3b85393a7
ByFanForFun <byfanforfun@proton.me>
2025-04-11 03:38:56 +0000

    Add game config override keys

--- src/game/gkioskconf.cc
+++ src/game/gkioskconf.cc
@@ -3,11 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "game/gconfig.h"
+
 #include "platform_compat.h"
 
 namespace fallout {
 
 Config kiosk_config;
+
+int _game_difficulty = 0;
+int _combat_difficulty = 0;
+int _language_filter = 0;
+
 static bool gkioskconf_initialized = false;
 static char gkioskconf_file_name[COMPAT_MAX_PATH];
 int gconfig_saveload_allowed;
@@ -32,9 +39,23 @@ bool gkioskconf_init()
     config_set_value(&kiosk_config, KIOSK_CONFIG_GAME_KEY, KIOSK_CONFIG_TIMER_INACTIVE_1, 100);
     config_set_value(&kiosk_config, KIOSK_CONFIG_GAME_KEY, KIOSK_CONFIG_TIMER_INACTIVE_2, 30);
     config_set_value(&kiosk_config, KIOSK_CONFIG_GAME_KEY, KIOSK_CONFIG_TIMER_INACTIVE_3, 10);
+    config_set_value(&kiosk_config, KIOSK_CONFIG_GAME_KEY, KIOSK_CONFIG_START_MESSAGE, 1);
+
+    config_set_value(&kiosk_config, KIOSK_CONFIG_OVERRIDE_KEY, KIOSK_CONFIG_OVERRIDE_GAME, 0);
+    config_set_value(&kiosk_config, KIOSK_CONFIG_OVERRIDE_KEY, KIOSK_CONFIG_OVERRIDE_COMBAT, 0);
+    config_set_value(&kiosk_config, KIOSK_CONFIG_OVERRIDE_KEY, KIOSK_CONFIG_OVERRIDE_LFILTER, 0);
 
     strcpy(gkioskconf_file_name, KIOSK_CONFIG_FILE_NAME);
     config_load(&kiosk_config, gkioskconf_file_name, false);
+
+    //override fallout.cfg
+    config_get_value(&kiosk_config, KIOSK_CONFIG_OVERRIDE_KEY, KIOSK_CONFIG_OVERRIDE_GAME, &_game_difficulty);
+    config_get_value(&kiosk_config, KIOSK_CONFIG_OVERRIDE_KEY, KIOSK_CONFIG_OVERRIDE_COMBAT, &_combat_difficulty);
+    config_get_value(&kiosk_config, KIOSK_CONFIG_OVERRIDE_KEY, KIOSK_CONFIG_OVERRIDE_LFILTER, &_language_filter);
+
+    config_set_value(&game_config, GAME_CONFIG_PREFERENCES_KEY, GAME_CONFIG_GAME_DIFFICULTY_KEY, _game_difficulty);
+    config_set_value(&game_config, GAME_CONFIG_PREFERENCES_KEY, GAME_CONFIG_COMBAT_DIFFICULTY_KEY, _combat_difficulty);
+    config_set_value(&game_config, GAME_CONFIG_PREFERENCES_KEY, GAME_CONFIG_LANGUAGE_FILTER_KEY, _language_filter);
 
     //set global vars
     config_get_value(&kiosk_config, KIOSK_CONFIG_GAME_KEY, KIOSK_CONFIG_ENABLE_SAVELOAD, &gconfig_saveload_allowed);
