624ba27d7eab8c166b78b12eb733f7d6de3e9ce5
ByFanForFun <byfanforfun@proton.me>
2025-06-10 12:42:59 +0000

    Add start inventory logic and build process

---
+++ src/game/start_inv_v.h
@@ -0,0 +1,11 @@
+#ifndef FALLOUT_GAME_START_INV_H_
+#define FALLOUT_GAME_START_INV_H_
+
+namespace fallout {
+
+#define DIALOG_BACKGROUND_HEIST 13
+
+int start_inventory();
+}
+
+#endif //FALLOUT_GAME_START_INV_H_
