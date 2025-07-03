cd08621ca17a20860b82eefedd259904a504d7f6
byfanforfun <byfanforfun@proton.me>
2025-07-03 23:33:51 +0000

    Add input rebind logic

---
+++ src/plib/gnw/input_rebind.h
@@ -0,0 +1,44 @@
+#ifndef FALLOUT_PLIB_GNW_INPUT_REBIND_H_
+#define FALLOUT_PLIB_GNW_INPUT_REBIND_H_
+
+namespace fallout {
+
+#define BIND_SECTION_NO_SEC    "nosec"
+
+typedef enum BindScreen {
+    SCREEN_MAIN,
+    SCREEN_GAME,
+    SCREEN_CHAR,
+    SCREEN_INV,
+    SCREEN_PIP,
+    SCREEN_MAX
+} BindScreen;
+
+typedef enum BindSection {
+    Main,
+    Game,
+    Editor,
+    Inv,
+    Pip,
+    BindSection_max
+} BindSection;
+
+/*
+#define BIND_SCREEN_MAIN    0
+#define BIND_SCREEN_GAME    1
+#define BIND_SCREEN_CHAR    2
+#define BIND_SCREEN_INV     3
+#define BIND_SCREEN_PIP     4
+
+#define BIND_SECTION_MAIN    "main"
+#define BIND_SECTION_GAME    "game"
+#define BIND_SECTION_CHAR    "char"
+#define BIND_SECTION_INV     "inv"
+#define BIND_SECTION_PIP     "pip"
+*/
+bool bind_init();
+int get_key(int screen, int key);
+
+}
+
+#endif //FALLOUT_PLIB_GNW_INPUT_REBIND_H_
