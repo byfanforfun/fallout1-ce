2f0562b85da18f7b41461625bb5f7753d383dee4
byfanforfun <byfanforfun@proton.me>
2025-07-03 22:06:43 +0000

    Add system_exec() func
    Signed-off-by: ByFanForFun <byfanforfun@proton.me>

---
+++ src/plib/gnw/system_exec.h
@@ -0,0 +1,10 @@
+#ifndef FALLOUT_PLIB_GNW_SYSTEM_EXEC_H_
+#define FALLOUT_PLIB_GNW_SYSTEM_EXEC_H_
+
+namespace fallout {
+
+bool system_exec(int line_nums[]);
+
+}
+
+#endif //FALLOUT_PLIB_GNW_SYSTEM_EXEC_H_
