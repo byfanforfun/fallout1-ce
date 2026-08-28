bc2184e2afd7e1cec9723c62aefe2e7beabe4440
byfanforfun <byfanforfun@proton.me>
2026-08-28 11:35:28 +0000

    fix: prefill kiosk save description only for continues mode

--- src/game/loadsave.cc
+++ src/game/loadsave.cc
@@ -2373,7 +2373,7 @@ static int GetComment(int a1)
     char description[LOAD_SAVE_DESCRIPTION_LENGTH];
     if (LSstatus[slot_cursor] == SLOT_STATE_OCCUPIED) {
         strncpy(description, LSData[a1].description, LOAD_SAVE_DESCRIPTION_LENGTH);
-    } else if (gconfig_continues_play > 0 || gconfig_saveload_disabled != 0) {
+    } else if (gconfig_continues_play > 0) {
         kiosk_build_save_description(description, sizeof(description));
     } else {
         description[0] = '\0';
