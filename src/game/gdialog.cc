1e6f8efa0f04b2698a234142f24242d32d06f61b
byfanforfun <byfanforfun@proton.me>
2026-09-16 21:18:58 +0000

    vkb: map 's' to backspace (delete character)

--- src/game/gdialog.cc
+++ src/game/gdialog.cc
@@ -3993,6 +3993,8 @@ static void about_loop()
                     }
                     continue;
                 }
+            } else if (keyCode == KEY_LOWERCASE_S || keyCode == KEY_UPPERCASE_S) {
+                keyCode = KEY_BACKSPACE;
             } else if (keyCode == KEY_LOWERCASE_I || keyCode == KEY_UPPERCASE_I) {
                 keyCode = KEY_SPACE;
             } else if (keyCode == KEY_LOWERCASE_C || keyCode == KEY_UPPERCASE_C) {
